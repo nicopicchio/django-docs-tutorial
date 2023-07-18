8286c15ef2a5905147415d0d4a233cc3710cf62f
Nico Picchio <70696511+nicopicchio@users.noreply.github.com>
2023-07-18 17:13:32 +0100

    creates detail view and updates index view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -8,7 +8,9 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s"
